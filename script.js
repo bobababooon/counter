20bf54f5654dfd266bf1b5be719a539edc52bc7c
bobababooon <bobababoon1230@gmail.com>
2026-01-24 10:19:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ const minus = document.getElementById("minus");
 const reset = document.gerElementById("reset");
 
 plus.addEventListener("click", () => {
-  number=number+1;
+  number=number + 1;
   count.textContent = number;
 });
 minus.addEventListener("click", () =>{
-  number=number - 1<;
+  number=number - 1;
+  count.textContent = number;
+});
+reset.addEventLestener("click", () =>{
+  number=0;
   count.textContent = number;
 });
