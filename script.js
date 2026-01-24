7aeff7090ced4c43c028b2a800753e7cf99bb1c4
bobababooon <bobababoon1230@gmail.com>
2026-01-24 10:12:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let number = 0;
 
 const count = document.getElementById("count");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const reset = document.gerElementById("reset");
+
+plus.addEventListener("click", () => {
+  number=number+1;
+  count.texrContent = number;
+});
